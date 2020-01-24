fb6ea7e87264f9d8cc6ec53ce593efc739c8ef92
ChandrakalaKirasur <chandrakalakirasur67@gmail.com>
2020-01-24 14:34:44 +0530

    address oo prog 12

--- PersonBean.java
+++ PersonBean.java
@@ -2,20 +2,13 @@ package com.blz.oops.model;
 
 public class PersonBean {
 	private String name;
-	private AddressBean address;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
 	private String phoneNum;
 	public PersonBean() {
 		
-	}
-	@Override
-	public String toString() {
-		return "Person name=" + name + ", address: " + address + ", phoneNum=" + phoneNum ;
-	}
-	public PersonBean(String name, AddressBean address, String phoneNum) {
-		super();
-		this.name = name;
-		this.address = address;
-		this.phoneNum = phoneNum;
 	}
 	public String getName() {
 		return name;
@@ -23,11 +16,29 @@ public class PersonBean {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public AddressBean getAddress() {
+	public String getAddress() {
 		return address;
 	}
-	public void setAddress(AddressBean address) {
+	public void setAddress(String address) {
 		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
 	}
 	public String getPhoneNum() {
 		return phoneNum;
@@ -35,4 +46,19 @@ public class PersonBean {
 	public void setPhoneNum(String phoneNum) {
 		this.phoneNum = phoneNum;
 	}
+	public PersonBean(String name, String address, String city, String state, String zip, String phoneNum) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNum = phoneNum;
+	}
+	@Override
+	public String toString() {
+		return "name= " + name + " , address=" + address + " , city=" + city + " , state=" + state + " , zip="
+				+ zip + " , phoneNum=" + phoneNum;
+	}
+	
 }
